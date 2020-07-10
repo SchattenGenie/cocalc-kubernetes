daae681db5675235e29f4915ea21cd8f4d533acb
Vladislav Belavin <belavin@phystech.edu>
2020-07-10 09:55:33 +0000

    update

--- server/backup.sh
+++ server/backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #$(kubectl get pod -l "app=cocalc" -o name)
-kubectl exec  -ti  cocalc-7b97f4465b-8sdcj  --  \
+kubectl --kubeconfig=/home/vbelavin/.kube/config exec  -ti  $(kubectl get pod -l "app=cocalc" -o name)  --  \
    bash -c "pg_dump -U sage -h /projects/postgres/data/socket/ smc > /backup/`date +%Y_%m_%d__%H_%M_%S`_smc.sql && tar -cf /backup/`date +%Y_%m_%d__%H_%M_%S`_smc.tar /projects/conf /projects/home"
 
